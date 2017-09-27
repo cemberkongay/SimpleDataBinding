02e41c8ed0d436f599be1baaa5c8b0a1451029bb
Berk Öngay <cberkongay@gmail.com>
2017-09-27 12:54:05 +0300

    Add project files.

---
+++ SimpleDataBinding/DataBinding.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="DataBinding.aspx.cs" Inherits="SimpleDataBinding.DataBinding" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+     <h1>Data Binding Example</h1>
+        <h4>
+            <asp:Literal ID="ltError" runat="server" ></asp:Literal>
+        </h4>
+        <asp:GridView ID="gvColor" CssClass="table table-striped" runat="server" AutoGenerateColumns="false" OnRowDeleting="gvColor_RowDeleting" OnRowEditing="gvColor_RowEditing" OnRowUpdating="gvColor_RowUpdating" OnRowCancelingEdit="gvColor_RowCancelingEdit">
+            <Columns>
+                <asp:TemplateField>
+                    <ItemTemplate>
+                        <asp:HiddenField ID="hdnColorId" runat="server" Value='<%#DataBinder.Eval(Container.DataItem, "ColorID") %>' />
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:BoundField DataField="Name" HeaderText="Name" />
+                <asp:BoundField DataField="HexCode" HeaderText="Hex Code" />
+                <asp:TemplateField HeaderText="Color Swatch">
+                    <ItemTemplate>
+                        <div class="color-swatch" >
+                            <div class="color-swatch" style='<%# "background-color:#" + Eval("HexCode") + ";"  %>' >
+                                <label></label>
+                            </div>
+                        </div>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:CommandField ShowEditButton="true" />
+                <asp:CommandField ShowDeleteButton="true" />
+            </Columns>
+        </asp:GridView>
+        <div class="row color-table">
+            <asp:Button ID="btnAddRow" runat="server" Text="Add New Row" CssClass="btn btn-primary" OnClick="btnAddRow_Click" />
+        </div>
+</asp:Content>
---
+++ SimpleDataBinding/DataBinding.aspx.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SimpleDataBinding
+{
+    public partial class DataBinding : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+                BindDataToGridView();
+        }
+
+        protected void gvColor_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            ltError.Text = string.Empty;
+            GridViewRow gvRow = (GridViewRow)gvColor.Rows[e.RowIndex];
+            HiddenField hdnColorId = (HiddenField)gvRow.FindControl("hdnColorId");
+            var connectionFormConfiguration = WebConfigurationManager.ConnectionStrings["DbConnection"];
+            using (SqlConnection dbConncetion = new SqlConnection(connectionFormConfiguration.ConnectionString))
+            {
+                try
+                {
+                    dbConncetion.Open();
+                    string sql = string.Format("DELETE FROM Colors WHERE ColorID={0}", hdnColorId.Value);
+                    SqlCommand cmd = new SqlCommand(sql, dbConncetion);
+                    cmd.ExecuteNonQuery();
+                    gvColor.EditIndex = -1;
+                    BindDataToGridView();
+                }
+                catch (Exception ex)
+                {
+                    ltError.Text = ex.Message;
+                }
+                finally
+                {
+                    dbConncetion.Close();
+                    dbConncetion.Dispose();
+                }
+            }
+        }
+
+        protected void gvColor_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            ltError.Text = string.Empty;
+            gvColor.EditIndex = e.NewEditIndex;
+            BindDataToGridView();
+        }
+
+        protected void gvColor_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            ltError.Text = string.Empty;
+            GridViewRow gvRow = gvColor.Rows[e.RowIndex];
+            HiddenField hdnColorId = (HiddenField)gvRow.FindControl("hdnColorId");
+            TextBox txtName = (TextBox)gvRow.Cells[1].Controls[0];
+            TextBox txtHex = (TextBox)gvRow.Cells[2].Controls[0];
+
+            var connectionFromConfiguration = WebConfigurationManager.ConnectionStrings["DbConnection"];
+            using (SqlConnection dbConnection = new SqlConnection(connectionFromConfiguration.ConnectionString))
+            {
+                try
+                {
+                    dbConnection.Open();
+                    string sql = string.Format("UPDATE Colors SET Name='{0}', HexCode='{1}' WHERE ColorID={2}", txtName.Text, txtHex.Text, hdnColorId.Value);
+                    SqlCommand cmd = new SqlCommand(sql, dbConnection);
+                    cmd.ExecuteNonQuery();
+                    gvColor.EditIndex = -1;
+                    BindDataToGridView();
+                }
+                catch (SqlException ex)
+                {
+                    ltError.Text = "Error:" + ex.Message;
+                }
+                finally
+                {
+                    dbConnection.Close();
+                    dbConnection.Dispose();
+                }
+            }
+        }
+
+        protected void gvColor_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            gvColor.EditIndex = -1;
+            BindDataToGridView();
+        }
+
+        public void BindDataToGridView()
+        {
+            var connectionFromConfiguration = WebConfigurationManager.ConnectionStrings["DbConnection"];
+            using (SqlConnection dbConnection = new SqlConnection(connectionFromConfiguration.ConnectionString))
+            {
+                try
+                {
+                    dbConnection.Open();
+                    SqlCommand command = new SqlCommand("SELECT ColorID, Name, HexCode FROM Colors ORDER BY ColorID", dbConnection);
+                    SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
+                    DataSet dataSet = new DataSet();
+                    dataAdapter.Fill(dataSet);
+                    if (dataSet.Tables[0].Rows.Count > 0)
+                    {
+                        gvColor.DataSource = dataSet;
+                        gvColor.DataBind();
+                    }
+                }
+                catch (SqlException ex)
+                {
+                    ltError.Text = "Error:" + ex.Message;
+                }
+                finally
+                {
+                    dbConnection.Close();
+                    dbConnection.Dispose();
+                }
+            }
+        }
+
+        protected void btnAddRow_Click(object sender, EventArgs e)
+        {
+            var connectionFromConfiguration = WebConfigurationManager.ConnectionStrings["DbConnection"];
+            using (SqlConnection dbConnection = new SqlConnection(connectionFromConfiguration.ConnectionString))
+            {
+                try
+                {
+                    dbConnection.Open();
+                    SqlCommand command = new SqlCommand("INSERT INTO Colors (Name, HexCode) VALUES ('','')", dbConnection);
+                    command.ExecuteNonQuery();
+                    BindDataToGridView();
+
+                }
+                catch (Exception ex)
+                {
+                    ltError.Text = "Error:" + ex.Message;
+                }
+                finally
+                {
+                    dbConnection.Close();
+                    dbConnection.Dispose();
+                }
+            }
+        }
+    }
+}
